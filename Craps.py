3e51d411ba4793622a4ea9c96bdfa94ac96d4416
DrewCurrie <drew.currie@westtown.edu>
2016-10-18 13:46:56 -0400

    Update Craps.py

--- Craps.py
+++ Craps.py
@@ -1,14 +1,15 @@
 import random
 from random import randint
 
-die_1 = random.randint(1,7)
-die_2 = random.randint(1,7)
-total_roll = (die_1 + die_2)
+
 player_currency = "100"
 
 start_game = input("Would you like to start the game? ")
 
 while "y" == start_game[0].lower():
+    die_1 = random.randint(1,7)
+    die_2 = random.randint(1,7)
+    total_roll = (die_1 + die_2)
     roll = input("Roll dice. ")
     print(total_roll)
     if total_roll == 2 or total_roll == 3 or total_roll == 12:
